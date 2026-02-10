4c23062dae58c853742c6f07a9e51a79911cdc51
Sharath <sharathkota070@gmail.com>
2026-02-10 19:36:20 +0530

    monkey-lang lexer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module monkeylang
+
+go 1.25.6
---
+++ token/token.go
@@ -0,0 +1,68 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// ideally use token types as int or a byte, since we are not
+// concerned about the perf. now... this way work!
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// indentifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETRUN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
